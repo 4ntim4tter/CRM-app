6c479e4f80668656f7c4237df8a4869ca9da99ff
Anes <4ntim4tter@gmail.com>
2023-03-07 11:21:31 +0100

    added sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,11 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
 
+    <body class="font-sans antialiased inline-flex" style="width: 100%">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900" style="width:20%;"><x-sidebar/></div>
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900" style="width: 100%;">
+            @include('layouts.navigation')
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white dark:bg-gray-800 shadow">
