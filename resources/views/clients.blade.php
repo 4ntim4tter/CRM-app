eeb121ee7e6a5b88a023e1e3004fad9368148ca0
Anes <4ntim4tter@gmail.com>
2023-03-16 09:32:51 +0100

    added css to user table

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -2,9 +2,26 @@
     <x-slot name="header">
         <x-header header='Clients' />
     </x-slot>
-    <div class="px-4 text-gray-800 dark:text-gray-200 leading-tight" style="max-height: 250px">
-        @foreach ($clients as $index => $client)
-            <p>{{ $index + 1 }}. {{ $client->name }}</p>
-        @endforeach
-    </div>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead >
+                    <tr>
+                        <th scope="col" class="px-6 py-3">ID</th>
+                        <th scope="col" class="px-6 py-3">Name</th>
+                        <th scope="col" class="px-6 py-3">Created at:</th>
+                        <th scope="col" class="px-6 py-3">Updated at:</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($clients as $client)
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <th class="scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"">{{ $client->id }}</th>
+                            <td class="px-6 py-4">{{ $client->name }}</td>
+                            <td class="px-6 py-4">{{ $client->created_at }}</td>
+                            <td class="px-6 py-4">{{ $client->updated_at }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
 </x-app-layout>
