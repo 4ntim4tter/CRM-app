c2a8f44dc8ab0dd65042b69e8eabade473bad910
Anes <4ntim4tter@gmail.com>
2023-04-03 14:14:42 +0200

    fixed request fields not returning select fields

--- resources/views/components/project-form.blade.php
+++ resources/views/components/project-form.blade.php
@@ -2,7 +2,7 @@
     <form method="post" action="{{ route('project.store', ['id' => $project->id]) }}" enctype="multipart/form-data">
         @csrf
         @method('post')
-
+        {{-- {{ dd($project->client_name) }} --}}
         <div class="pb-1">
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $project->name)" required
@@ -11,8 +11,12 @@
         </div>
 
         <div class="pb-1">
-            <x-input-label for="clients" :value="__('Client')" />
-            <x-select-form id="clients" name="clients" :selectoption="$clients" />
+            <x-input-label for="client" :value="__('Client')" />
+            <select name="client" id="client" class='form-control border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm'>
+                @foreach ($clients as $option)
+                    <option value="{{ $option }}" @if($project->client_name === $option) selected @endif>{{ $option }}</option>
+                @endforeach
+            </select>
         </div>
 
         <div class="pb-1">
@@ -24,8 +28,12 @@
         </div>
 
         <div class="pb-1">
-            <x-input-label for="users" :value="__('User')" />
-            <x-select-form id="users" name="users" :selectoption="$users" />
+            <x-input-label for="user" :value="__('User')" />
+            <select name="user" id="user" class='form-control border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm'>
+                @foreach ($users as $option)
+                    <option value="{{ $option }}" @if($project->user_name === $option) selected @endif>{{ $option }}</option>
+                @endforeach
+            </select>
         </div>
 
         <div class="flex items-center gap-4 py-2">
@@ -45,8 +53,12 @@
         </div>
 
         <div class="pb-1">
-            <x-input-label for="clients" :value="__('Client')" />
-            <x-select-form id="clients" name="clients" :selectoption="$clients" />
+            <x-input-label for="client" :value="__('Client')" />
+            <select name="client" id="client" class='form-control border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm'>
+                @foreach ($clients as $option)
+                    <option value="{{ $option }}">{{ $option }}</option>
+                @endforeach
+            </select>
         </div>
 
         <div class="pb-1">
@@ -57,8 +69,12 @@
         </div>
 
         <div class="pb-1">
-            <x-input-label for="users" :value="__('User')" />
-            <x-select-form id="users" name="users" :selectoption="$users" />
+            <x-input-label for="user" :value="__('User')" />
+            <select name="user" id="user" class='form-control border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm'>
+                @foreach ($users as $option)
+                    <option value="{{ $option }}">{{ $option }}</option>
+                @endforeach
+            </select>
         </div>
 
         <div class="flex items-center gap-4 py-2">
--- resources/views/components/select-form.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['disabled' => false])
-@aware(['selectoption'])
-
-<select class='border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm'>
-    @foreach ($selectoption as $option)
-        <option>{{ $option }}</option>
-    @endforeach
-</select>
