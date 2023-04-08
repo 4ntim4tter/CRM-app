c0df3b0e6c3ae1e077853836f3d404de277fe694
4ntim4tter <4ntim4tter@gmail.com>
2023-04-09 01:17:02 +0200

    added trashed button to projects

--- resources/views/components/project-table.blade.php
+++ resources/views/components/project-table.blade.php
@@ -1,8 +1,19 @@
 @aware(['projects'])
 
 <div class="p-1 relative overflow-x-auto shadow-md sm:rounded-lg">
+
+    <label class="relative inline-flex items-center cursor-pointer float-right">
+        <input id="checkbox" name="checkbox" type="checkbox" class="sr-only peer">
+        <div
+            class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+        </div>
+        <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Show Trashed
+        </span>
+    </label>
+
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead >
+        <thead>
             <tr>
                 <th scope="col" class="px-6 py-3">ID</th>
                 <th scope="col" class="px-6 py-3">Name</th>
@@ -15,20 +26,34 @@
         </thead>
         <tbody>
             @foreach ($projects as $project)
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <tr
+                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                     <form action="{{ route('project.delete', $project) }}" method='post'>
                         @csrf
                         @method('delete')
-                        <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $project->id }}</th>
-                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $project->name }}</td>
-                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white" style="white-space: nowrap; max-width: 150px; overflow-x:hidden">{{ $project->client_name }}</td>
-                        <td class="px-6 py-4" style="white-space: nowrap; max-width: 150px; overflow-x:hidden;" title="{{ $project->description }}">{{ $project->description }}</td>
+                        <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $project->id }}</th>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $project->name }}</td>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                            style="white-space: nowrap; max-width: 150px; overflow-x:hidden">{{ $project->client_name }}
+                        </td>
+                        <td class="px-6 py-4" style="white-space: nowrap; max-width: 150px; overflow-x:hidden;"
+                            title="{{ $project->description }}">{{ $project->description }}</td>
                         <td class="px-6 py-4">{{ $project->user_name }}</td>
-                        <td class="px-6 py-4">@if ($project->status) Open @else Closed @endif</td>
+                        <td class="px-6 py-4">
+                            @if ($project->status)
+                                Open
+                            @else
+                                Closed
+                            @endif
+                        </td>
                         <td class="px-6 py-4">{{ $project->deadline }}</td>
                         <td class="px-6 py-4 inline-block">
-                            <a href="{{ route('project.edit', $project) }}"class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">Edit</a>
-                            <x-danger-button onclick="return confirm ('Are you sure?')">{{ __('Delete') }}</x-danger-button>
+                            <a
+                                href="{{ route('project.edit', $project) }}"class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">Edit</a>
+                            <x-danger-button onclick="return confirm ('Are you sure?')">{{ __('Delete') }}
+                            </x-danger-button>
                         </td>
                     </form>
                 </tr>
--- resources/views/project-edit.blade.php
+++ resources/views/project-edit.blade.php
@@ -1,29 +1,29 @@
 <x-app-layout>
-    @if(Route::is('project.edit'))
+    @if (Route::is('project.edit'))
         <x-slot name="header">
-            <x-header header='Edit Project'/>
+            <x-header header='Edit Project' />
         </x-slot>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                     <div class="max-w-xl">
-                        <x-project-form :project="$project" :clients="$client_names" :users="$user_names"/>
+                        <x-project-form :project="$project" :clients="$client_names" :users="$user_names" />
                     </div>
                 </div>
             </div>
         </div>
     @endif
-    @if(Route::is('project.create'))
+    @if (Route::is('project.create'))
         <x-slot name="header">
-            <x-header header='Create Project'/>
+            <x-header header='Create Project' />
         </x-slot>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                     <div class="max-w-xl">
-                        <x-project-form :clients="$client_names" :users="$user_names"/>
+                        <x-project-form :clients="$client_names" :users="$user_names" />
                     </div>
                 </div>
             </div>
