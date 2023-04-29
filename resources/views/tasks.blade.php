f6cde6e0353a31863957dab2729a8f76eca7532d
4ntim4tter <4ntim4tter@gmail.com>
2023-04-29 11:25:52 +0200

    what

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -10,7 +10,7 @@
             </div>
         @endif
     </div>
-    <x-dropdown align="bottom" width="48" contentClasses="py-1 dark:bg-slate-50" position="fixed ml-7 mt-1">
+    <x-dropdown align="bottom" width="48" contentClasses="py-1 dark:bg-slate-50" position="relative w-48">
         <x-slot name="trigger">
             <a href="#"
                 class="inline-flex items-center px-4 py-2 ml-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md dark:bg-gray-200 dark:text-gray-800 hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800">Select
